a2366c975c3b3021051c5d8ecd4e6a4f0f172356
Kevin Tardif <kevin@vfpowertech.com>
2016-01-25 10:36:18 -0500

    Calling WebEngine::executeScript in main thread

--- desktop/src/main/kotlin/com/vfpowertech/jsbridge/desktop/JFXWebEngineInterface.kt
+++ desktop/src/main/kotlin/com/vfpowertech/jsbridge/desktop/JFXWebEngineInterface.kt
@@ -2,12 +2,16 @@ package com.vfpowertech.jsbridge.desktop
 
 import com.vfpowertech.jsbridge.core.dispatcher.Dispatcher
 import com.vfpowertech.jsbridge.core.dispatcher.WebEngineInterface
+import javafx.application.Platform
 import javafx.scene.web.WebEngine
 import netscape.javascript.JSObject
 
 class JFXWebEngineInterface(private val engine: WebEngine) : WebEngineInterface {
     override fun runJS(js: String) {
-        engine.executeScript(js)
+        //executeScript must be called from the main thread
+        Platform.runLater {
+            engine.executeScript(js)
+        }
     }
 
     override fun register(dispatcher: Dispatcher) {
