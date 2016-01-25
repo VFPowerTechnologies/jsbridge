1f8c62101bdaccf32d0bb501b07b920a4493825e
Kevin Tardif <kevin@vfpowertech.com>
2016-01-25 11:15:09 -0500

    Slightly modified JFXWebEngineInterface.runJS

--- desktop/src/main/kotlin/com/vfpowertech/jsbridge/desktop/JFXWebEngineInterface.kt
+++ desktop/src/main/kotlin/com/vfpowertech/jsbridge/desktop/JFXWebEngineInterface.kt
@@ -5,13 +5,25 @@ import com.vfpowertech.jsbridge.core.dispatcher.WebEngineInterface
 import javafx.application.Platform
 import javafx.scene.web.WebEngine
 import netscape.javascript.JSObject
+import org.slf4j.LoggerFactory
 
 class JFXWebEngineInterface(private val engine: WebEngine) : WebEngineInterface {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     override fun runJS(js: String) {
         //executeScript must be called from the main thread
-        Platform.runLater {
+        if (Platform.isFxApplicationThread())
+            //propagate any exceptions to caller immediately
             engine.executeScript(js)
-        }
+        else
+            Platform.runLater {
+                try {
+                    engine.executeScript(js)
+                }
+                catch (e: Throwable) {
+                    log.error("Script execution failed for: {}", js, e)
+                }
+            }
     }
 
     override fun register(dispatcher: Dispatcher) {
