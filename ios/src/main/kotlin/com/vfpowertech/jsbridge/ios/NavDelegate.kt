62f8ed431a03a5d1a0c824dd87a4dc5bb1c0161b
Kevin Tardif <kevin@vfpowertech.com>
2016-01-27 10:07:35 -0500

    Added java->js test running to ios

--- ios/src/main/kotlin/com/vfpowertech/jsbridge/ios/NavDelegate.kt
+++ ios/src/main/kotlin/com/vfpowertech/jsbridge/ios/NavDelegate.kt
@@ -1,22 +1,23 @@
 package com.vfpowertech.jsbridge.ios
 
-import com.vfpowertech.jsbridge.core.services.js.JSServiceImpl
-import com.vfpowertech.jsbridge.core.services.js.V
+import com.vfpowertech.jsbridge.core.dispatcher.WebEngineInterface
+import com.vfpowertech.jsbridge.core.services.js.JSTestService
+import com.vfpowertech.jsbridge.core.services.js.testing.TestRunner
 import org.robovm.apple.foundation.Foundation
 import org.robovm.apple.webkit.*
 import org.robovm.objc.annotation.Block
 import org.robovm.objc.block.VoidBlock1
 import org.slf4j.LoggerFactory
 
-class NavDelegate(private val jsService: JSServiceImpl) : WKNavigationDelegateAdapter() {
+class NavDelegate(
+        private val engineInterface: WebEngineInterface,
+        private val jsTestService: JSTestService
+) : WKNavigationDelegateAdapter() {
     private val log = LoggerFactory.getLogger(javaClass)
     override fun didFinishNavigation(webView: WKWebView, navigation: WKNavigation) {
         log.info("Navigation completed")
-        jsService.syncFn(V(5, 6), 5) success {
-            log.info("Result of syncFn: {}", it)
-        } fail {
-            log.info("syncFn failed: {}", it)
-        }
+        val testRunner = TestRunner(engineInterface, jsTestService)
+        Thread(testRunner).start()
     }
 
     override fun decidePolicyForNavigationAction(webView: WKWebView, navigationAction: WKNavigationAction, @Block decisionHandler: VoidBlock1<WKNavigationActionPolicy>) {
--- ios/src/main/kotlin/com/vfpowertech/jsbridge/ios/WebViewController.kt
+++ ios/src/main/kotlin/com/vfpowertech/jsbridge/ios/WebViewController.kt
@@ -1,16 +1,10 @@
 package com.vfpowertech.jsbridge.ios
 
 import com.vfpowertech.jsbridge.core.dispatcher.Dispatcher
-import com.vfpowertech.jsbridge.core.services.js.JSServiceImpl
-import com.vfpowertech.jsbridge.core.services.TestService
-import com.vfpowertech.jsbridge.core.services.SampleServiceJSProxy
 import org.robovm.apple.foundation.NSBundle
 import org.robovm.apple.foundation.NSURL
 import org.robovm.apple.foundation.NSURLRequest
-import org.robovm.apple.uikit.UIColor
-import org.robovm.apple.uikit.UIScreen
-import org.robovm.apple.uikit.UIView
-import org.robovm.apple.uikit.UIViewController
+import org.robovm.apple.uikit.*
 import org.robovm.apple.webkit.WKUserContentController
 import org.robovm.apple.webkit.WKWebView
 import org.robovm.apple.webkit.WKWebViewConfiguration
@@ -30,19 +24,26 @@ class WebViewController : UIViewController() {
 
         val webView = WKWebView(contentView.frame, configuration)
 
-        val dispatcher = Dispatcher(IOSWebEngineInterface(webView))
+        val engineInterface = IOSWebEngineInterface(webView)
+        val dispatcher = Dispatcher(engineInterface)
 
-        val sampleService = com.vfpowertech.jsbridge.core.services.TestService()
-        dispatcher.registerService("SampleService", com.vfpowertech.jsbridge.core.services.SampleServiceJSProxy(sampleService, dispatcher))
+        val testService = com.vfpowertech.jsbridge.core.services.TestService()
+        dispatcher.registerService("TestService", com.vfpowertech.jsbridge.core.services.jstojava.TestServiceToJavaProxy(testService, dispatcher))
 
-        val jsService = JSServiceImpl(dispatcher)
+        val jsTestService = com.vfpowertech.jsbridge.core.services.js.javatojs.JSTestServiceToJSProxy(dispatcher)
 
-        webView.navigationDelegate = NavDelegate(jsService)
+        webView.navigationDelegate = NavDelegate(engineInterface, jsTestService)
 
         val path = NSBundle.getMainBundle().findResourcePath("index", "html")
         val f = File(path)
         webView.loadRequest(NSURLRequest(NSURL(f)))
 
         contentView.addSubview(webView)
+
+        val runTestsBtn = UIButton()
+        runTestsBtn.addOnTouchUpInsideListener({ control, ev ->
+
+        })
+        contentView.addSubview(runTestsBtn)
     }
 }
