5f752eec340a85bc0dad3fa4726eb09cc1f46b1e
Kevin Tardif <kevin@vfpowertech.com>
2016-02-03 09:23:05 -0500

    AndroidWebEngineInterface now calling dispatcher in main thread only

--- android-webengine/src/main/kotlin/com/vfpowertech/jsbridge/androidwebengine/AndroidWebEngineInterface.kt
+++ android-webengine/src/main/kotlin/com/vfpowertech/jsbridge/androidwebengine/AndroidWebEngineInterface.kt
@@ -1,15 +1,27 @@
 package com.vfpowertech.jsbridge.androidwebengine
 
 import android.os.Handler
+import android.os.Looper
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 
 class AndroidWebEngineInterface(private val webView: WebView) : com.vfpowertech.jsbridge.core.dispatcher.WebEngineInterface {
     private lateinit var dispatcher: com.vfpowertech.jsbridge.core.dispatcher.Dispatcher
-    private val handler = Handler(webView.context.mainLooper)
+    private val mainLooper = webView.context.mainLooper
+    private val handler = Handler(mainLooper)
+
+    private fun isMainThread(): Boolean =
+        Looper.myLooper() == mainLooper
+
+    private inline fun runInMainThread(crossinline body: () -> Unit): Unit {
+        if (isMainThread())
+            body()
+        else
+            handler.post { body() }
+    }
 
     override fun runJS(js: String) {
-        handler.post { webView.evaluateJavascript(js, null) }
+        runInMainThread { webView.evaluateJavascript(js, null) }
     }
 
     override fun register(dispatcher: com.vfpowertech.jsbridge.core.dispatcher.Dispatcher) {
@@ -19,11 +31,15 @@ class AndroidWebEngineInterface(private val webView: WebView) : com.vfpowertech.
 
     @JavascriptInterface
     fun call(serviceName: String, methodName: String, methodArgs: String, callbackId: String) {
-        dispatcher.call(serviceName, methodName, methodArgs, callbackId)
+        runInMainThread {
+            dispatcher.call(serviceName, methodName, methodArgs, callbackId)
+        }
     }
 
     @JavascriptInterface
     fun callbackFromJS(callbackId: String, isError: Boolean, jsonRetVal: String) {
-        dispatcher.callbackFromJS(callbackId, isError, jsonRetVal)
+        runInMainThread {
+            dispatcher.callbackFromJS(callbackId, isError, jsonRetVal)
+        }
     }
 }
